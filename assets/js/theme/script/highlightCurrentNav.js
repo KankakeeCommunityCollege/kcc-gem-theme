f6b85e2df3146a3da13a7a014a8e8893c01de2d5
wdzajicek <tig.welder4@gmail.com>
2019-08-07 11:29:12 -0500

    Create custom JS to highlight user's location in sub-nav too!

--- assets/js/theme/script/highlightCurrentNav.js
+++ assets/js/theme/script/highlightCurrentNav.js
@@ -1,7 +1,21 @@
+// Custom Vanilla JS to highlight the user's current location in the navigation bar and the sub-nav navigiation bar
 function setActive(thisLink, linkText) {
   let thisAnchor = thisLink.querySelector('a');
   thisLink.classList.add('active');
   thisAnchor.insertAdjacentHTML('beforeend', ' <span class="sr-only">(current)</span>');
+}
+
+function highlightSubNav(url) {
+  const subNavItems = document.querySelectorAll('.js-sub-nav-item');
+  for ( let link of subNavItems) {
+    const anchor = link.querySelector('a');
+    const href = anchor.getAttribute('href').replace(/\.\.\//g, '');
+    const linkText = anchor.textContent;
+    const urlMatchesLink = url.indexOf(href) > -1;
+    urlMatchesLink ?
+      setActive(link, linkText)
+    : null;
+  }
 }
 
 function highlightNav() {
@@ -9,6 +23,9 @@ function highlightNav() {
   const url = window.location.href.replace(/(^\w+:|^)\/\//, '');
   const urlIsIndex = url === host || url === host + '#contact' ;
   const navigationItems = document.querySelectorAll('.js-nav-item');
+  document.getElementById('subNavNav') ?
+    highlightSubNav(url)
+  : null;
 
   for ( let link of navigationItems ) {
     const linkAnchor = link.querySelector('a');
