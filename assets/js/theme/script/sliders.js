0d104837f28f3c1d0070dc41a079af290d3e51b7
wdzajicek <tig.welder4@gmail.com>
2019-08-16 13:00:59 -0500

    Fix slider's custom next/prev arrows on pages other than home-page...
    Point the src attribute for the custom arrows to the sight root `/assets/img/dbl-next.svg` ( instead of a relative path of `assets/img/dpb-next.svg`)
    THat way if the slider is deeper in the site, the src attributes will always point to the correct location.

--- assets/js/theme/script/sliders.js
+++ assets/js/theme/script/sliders.js
@@ -6,8 +6,8 @@ function initSliders() {
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 3000,
-      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
-      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
+      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'
     });
   }
   document.querySelector('.hero-slider__slider') ? initSlick() : null;
