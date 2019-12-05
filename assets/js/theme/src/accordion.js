2eb3d2dad5743c7f86029af759ae68ec87aa5de5
wdzajicek <tig.welder4@gmail.com>
2019-12-05 15:41:55 -0600

    Filter out non-accordion related collapse stateChange events...
    Bug fix for `accordion.js` interfering with mobile nav menu toggler:
    1. Instead of watching for state changes on all `.collapse` elements (`.collapse` gets used in navbar too), exclude `.navbar-collapse` elements:
    ```javascript
    - $('.collapse').on(stateChange, function(e) {
    + $('.collapse:not(.navbar-collapse)').on(stateChange, function(e) {
    ```
    2. Add an "if" check to filter out all events that don't have an `#accordion` as the target's parent. This is
    to prevent future errors or interference with anything else using the Bootstrap 4 `.collapse()` method.
    ```javascript
    if ( e.target.parentNode.parentNode.id === 'accoridon' ) {
      // Proceed with `accordion.js` stuffs here:
      // ...
    }
    ```

--- assets/js/theme/src/accordion.js
+++ assets/js/theme/src/accordion.js
@@ -1,6 +1,7 @@
 const CLASS_TO_TOGGLE = 'accordion__icon--minus'; // A class defined in the stylesheet
 const ACCORDION_ICON_CLASS = '.accordion__icon'; // Specific class in the HTML (from `_includes/accordion.html`)!
 const COLLAPSE_STATES_ARR = ['show.bs.collapse', 'hide.bs.collapse']; // BOOTSTRAP 4 specific collapse states
+const COLLAPSE_JQUERY_SELECTOR = '.collapse:not(.navbar-collapse)';  // BOOTSTRAP 4 jQuery selector for callapseable divs
 
 function toggleClass(el) {
   return el.classList.toggle(CLASS_TO_TOGGLE);
@@ -13,10 +14,12 @@ function findAccordionIcon(parent) {
 }
 
 function onCollapseChangeFunction(stateChange) {
-  $('.collapse').on(stateChange, function (e) { // BOOTSTRAP 4 METHOD: https://getbootstrap.com/docs/4.3/components/collapse/#events
-    const COLLAPSE_PARENT = e.target.parentElement; // SPECIFIC TO THE BOOTSTRAP 4 COLLAPSE EVENT
-    //console.log(e.target.parentElement);
-    findAccordionIcon(COLLAPSE_PARENT);
+  $(COLLAPSE_JQUERY_SELECTOR).on(stateChange, function (e) { // BOOTSTRAP 4 METHOD: https://getbootstrap.com/docs/4.3/components/collapse/#events
+    if (e.target.parentNode.parentNode.id === 'accordion') { // FILTERS OUT COLLAPSE STATE_CHANGES THAT ARE NOT INSIDE AN ACCORDION
+      const COLLAPSE_PARENT = e.target.parentElement; // SPECIFIC TO THE BOOTSTRAP 4 COLLAPSE EVENT
+      //console.log(e);
+      findAccordionIcon(COLLAPSE_PARENT);
+    }
   });
 }
 
