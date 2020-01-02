753ed3fc49d99a6a9fc415861e7d46579f0e497f
wdzajicek <tig.welder4@gmail.com>
2020-01-02 11:43:14 -0600

    Add comments for `TODO:` items.

--- assets/js/theme/src/megaNav.js
+++ assets/js/theme/src/megaNav.js
@@ -29,6 +29,18 @@ function megaNavInit() {
   if ( !document.getElementById('headerGlobalNavbar') )
     return;
 
+  // TODO:
+  // 1.) Add bail-out if screen can never be wider than 992px:
+  // use `window.matchMedia(mediaQueryString).matches` method
+
+  // 2.) Use the "orientationchange event" to handle orentation changes on mobile devies:
+  // The "orientationchange" event will only work in mobile (not supported in any deskop browser)
+
+  // 3.) Add screen-size check:
+  // IF: screen is larger than 992px, fire JS to add mouseenter listener.
+  // ELSE: add screen resize event-listener & handler function
+
+  // 4.) Move this stuff into its own function:
   const navGlobalBottomEl = document.getElementById(NAV_GLOBAL_BOTTOM_ID_STRING);
   const navGlobalBottomBootstrapDropdownNodeList = navGlobalBottomEl.querySelectorAll('.nav-link.dropdown-toggle');
 
