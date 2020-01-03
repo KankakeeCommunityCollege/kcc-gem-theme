be66fe7bf4c0f52a68e56e402bdf4aa08156e391
wdzajicek <tig.welder4@gmail.com>
2020-01-03 14:42:58 -0600

    Fix news.kcc.edu double-highlighting in nav.

--- assets/js/theme/src/highlightCurrentNav.js
+++ assets/js/theme/src/highlightCurrentNav.js
@@ -1,17 +1,20 @@
-// Custom Vanilla JS to highlight the user's current location in the navigation bar and the sub-nav navigiation bar
+// Custom Vanilla JS to highlight the user's current location in the navigation bar and the sub-nav navigation bar
 function setActive(thisLink, linkText) {
   let thisAnchor = thisLink.querySelector('a');
+
   thisLink.classList.add('active');
   thisAnchor.insertAdjacentHTML('beforeend', ' <span class="sr-only">(current)</span>');
 }
 
-function highlightSubNav(url) {
+function highlightSubNav(pathname) {
   const subNavItems = document.querySelectorAll('.js-sub-nav-item');
+
   for ( let link of subNavItems) {
     const anchor = link.querySelector('a');
     const href = anchor.getAttribute('href').replace(/\.\.\//g, '');
     const linkText = anchor.textContent;
-    const urlMatchesLink = url.indexOf(href) > -1;
+    const urlMatchesLink = pathname.indexOf(href) !== -1;
+
     urlMatchesLink ?
       setActive(link, linkText)
     : null;
@@ -19,25 +22,27 @@ function highlightSubNav(url) {
 }
 
 function highlightNav() {
-  const host = window.location.host + '/';
-  const url = window.location.href.replace(/(^\w+:|^)\/\//, '');
-  const urlIsIndex = url === host || url === host + '#contact' ;
-  const navigationItems = document.querySelectorAll('.js-nav-item');
+  const pathname = window.location.pathname;
+  const locationIsContactHash = window.location.hash === '#contact';
+  const locationIsHome = window.location.pathname === '/';
+
   document.getElementById('subNavNav') ?
-    highlightSubNav(url)
+    highlightSubNav(pathname)
   : null;
+
+  const navigationItems = document.querySelectorAll('.js-nav-item');
 
   for ( let link of navigationItems ) {
     const linkAnchor = link.querySelector('a');
-    const linkHref = linkAnchor.getAttribute('href').replace(/\.\.\//g, '');
-    const linkText = linkAnchor.textContent;
-    const linkIsHome = linkText.toLowerCase() === 'home';
-    const urlMatchesLink = url.indexOf(linkHref) > -1;
+    const linkHrefValue = linkAnchor.getAttribute('href').replace(/\.\.\//g, '');
+    const linkTextValue = linkAnchor.textContent;
+    const linkIsHome = linkTextValue.toLowerCase() === 'home';
+    const urlMatchesLink = pathname.indexOf(linkHrefValue) !== -1;
 
-    if ( urlIsIndex ) {
-      linkIsHome ? setActive(link, linkText) : null;
+    if ( locationIsHome || locationIsContactHash ) {
+      linkIsHome ? setActive(link, linkTextValue) : null;
     } else {
-      urlMatchesLink && !linkIsHome ? setActive(link, linkText) : null;
+      urlMatchesLink && !linkIsHome ? setActive(link, linkTextValue) : null;
     }
   }
 }
