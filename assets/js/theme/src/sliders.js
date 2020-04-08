a6daf6edfaca36d11312f14ef370dc915cabfd5c
wdzajicek <tig.welder4@gmail.com>
2020-04-08 08:52:45 -0500

    Create button after slick initalizes...
    Create a succesfull "on-slick-initialization" function that creates and injects a new button element
    (to serve as a play/pause toggle for our slick sliders). Slick uses a jQuery style `.on('event', function())`
    function to detect slick events like swiping, or first initialization.
    
    To watch for slick's first initialization you have to call the code below, before initializing slick:
    ```javascript
    $('.my-slick-el').on('init', function(event, slick){ // code to execute after slick has initialized });
    ```
    
    See the [Events reference on Slick's GitHub](https://github.com/kenwheeler/slick/#events) at <https://github.com/kenwheeler/slick/#events>

--- assets/js/theme/src/sliders.js
+++ assets/js/theme/src/sliders.js
@@ -1,15 +1,43 @@
+function initSlick() {
+  $('.hero-slider__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow:'<img alt="Previous" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
+    nextArrow:'<img alt="Next" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'
+  });
+}
+
+function setAttributeOnEl(el, attr, value) {
+  return el.setAttribute(attr, value);
+}
+
+function createButton() {
+  const SLICK_PARENT_EL = document.querySelector('.hero-slider__slider');
+  const button = document.createElement('button');
+  const buttonText = 'Pause';
+
+  setAttributeOnEl(button, 'role', 'button');
+  setAttributeOnEl(button, 'type', 'button');
+  setAttributeOnEl(button, 'aria-label', 'Pause');
+  button.innerHTML = buttonText;
+  SLICK_PARENT_EL.appendChild(button);
+}
+
+function watchForSlickInit(initFunction) {
+  $('.hero-slider__slider').on('init', function(event, slick){
+    console.log(slick);
+    createButton();
+  });
+  initFunction();
+}
+
 function initSliders() {
-  function initSlick() {
-    $('.hero-slider__slider').slick({
-      dots: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
-      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'
-    });
-  }
-  document.querySelector('.hero-slider__slider') ? initSlick() : null;
+  if ( ! document.querySelector('.hero-slider__slider') )
+    return;
+
+  watchForSlickInit(initSlick);
 }
 export default initSliders;
