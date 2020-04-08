3063d3b16b413cbac60975159c89e1c1111b6929
wdzajicek <tig.welder4@gmail.com>
2020-04-08 11:40:14 -0500

    Cleanup `sliders.js` and add comments.

--- assets/js/theme/src/sliders.js
+++ assets/js/theme/src/sliders.js
@@ -1,12 +1,27 @@
+// Custom JS to initialize slick slider (https://github.com/kenwheeler/slick) and then build a play/pause toggle button
+// Uses slick-specific methods & slick events. See slick events at: https://github.com/kenwheeler/slick/#events
+const HERO_SLIDER_CLASSNAME = '.hero-slider__slider';  // Our classname for the hero-slider's DOM parent
+const SLICK_PLAY = 'slickPlay';  // Methods unique to slick
+const SLICK_PAUSE = 'slickPause';  // Methods unique to slick
+const SLICK_NEXT_SLIDE = 'slickNext';  // Methods unique to slick
+const play = 'Play';
+const pause = 'Pause';
+// =============================================================================================== //
+// TODO:                                                                                           //
+//                                                                                                 //
+//    Rewrite below prevArrow & nextArrow as <button> elements (like slick's default arrows).      //
+//                                                                                                 //
+// =============================================================================================== //
+
 function initSlick() {
-  $('.hero-slider__slider').slick({
+  $(HERO_SLIDER_CLASSNAME).slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
-    prevArrow:'<img alt="Previous" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
-    nextArrow:'<img alt="Next" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'
+    autoplaySpeed: 4000,
+    prevArrow:'<img alt="Previous" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',  // TODO: change to <button> el
+    nextArrow:'<img alt="Next" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'  // TODO: change to <button> el
   });
 }
 
@@ -14,30 +29,58 @@ function setAttributeOnEl(el, attr, value) {
   return el.setAttribute(attr, value);
 }
 
+function toggleSlickPlayState(el, slickState, newButtonText) {
+  const newButtonTextIsPause = newButtonText === pause;
+
+  $(HERO_SLIDER_CLASSNAME).slick(slickState);
+  setAttributeOnEl(el, 'aria-label', newButtonText);
+  el.innerHTML = newButtonText;
+
+  newButtonTextIsPause ? $(HERO_SLIDER_CLASSNAME).slick(SLICK_NEXT_SLIDE) : null;
+}
+
+function watchForElementClicks(el) {
+  el.addEventListener('click', function(e) {
+    let buttonTextIsPause = el.innerHTML === 'Pause';
+
+    buttonTextIsPause ? toggleSlickPlayState(el, SLICK_PAUSE, play) : toggleSlickPlayState(el, SLICK_PLAY, pause);
+  });
+}
+
 function createButton() {
-  const SLICK_PARENT_EL = document.querySelector('.hero-slider__slider');
+  const SLICK_PARENT_EL = document.querySelector(HERO_SLIDER_CLASSNAME);
   const button = document.createElement('button');
-  const buttonText = 'Pause';
+  const initialButtonText = 'Pause';
 
   setAttributeOnEl(button, 'role', 'button');
   setAttributeOnEl(button, 'type', 'button');
   setAttributeOnEl(button, 'aria-label', 'Pause');
-  button.innerHTML = buttonText;
+  button.innerHTML = initialButtonText;
+  button.classList.add('hero-slider__button--toggle');
   SLICK_PARENT_EL.appendChild(button);
+  watchForElementClicks(button);
 }
 
 function watchForSlickInit(initFunction) {
-  $('.hero-slider__slider').on('init', function(event, slick){
-    console.log(slick);
+  // slick's on 'init' function (See events in slick docs):
+  $(HERO_SLIDER_CLASSNAME).on('init', function(event, slick){ // According to slick doc's; you have to call a $(slick).on('init', function(){ //... }); before you initialize slick
     createButton();
   });
-  initFunction();
+  initFunction();  // Initializing slick after the above `.on('init', function() {})`
 }
 
 function initSliders() {
-  if ( ! document.querySelector('.hero-slider__slider') )
+  if ( ! document.querySelector(HERO_SLIDER_CLASSNAME) )
     return;
 
   watchForSlickInit(initSlick);
 }
+//
+//  USAGE:
+//
+//    import initSliders from './sliders.js';
+//
+//    document.addEventListener('DOMContentLoaded', function() {
+//      initSliders();
+//    });
 export default initSliders;
