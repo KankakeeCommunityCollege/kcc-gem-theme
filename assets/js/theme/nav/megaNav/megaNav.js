75428c5cc43560813d022d1c07e0f8e37ebd8755
wdzajicek <tig.welder4@gmail.com>
2020-07-02 17:25:58 -0500

    Add custom JS to underline the current site in the navigation...
    Create a JS module to underline the current site in the navigation
    i.e. If user is anywhere in /academics site, "Academics" link is underlined in the nav
    
    This feature addition is to provide the user with a visual reference as to where they are in the site
    Breadcrumbs without the breadcurmbs.

--- assets/js/theme/nav/megaNav/megaNav.js
+++ assets/js/theme/nav/megaNav/megaNav.js
@@ -2,10 +2,12 @@ import toggleMenuOnWindowResize from './toggleMenuOnWindowResize.js';
 import toggleDropdownOnWindowResize from './toggleDropdownOnWindowResizeTwo.js';
 import googleCustomSearchInit from './googleCustomSearch.js';
 import closeMenuOnClick from './closeMegaNavOnClick.js';
+import underlineCurrentSite from './underlineCurrentSite.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+  googleCustomSearchInit();
+  underlineCurrentSite();
   toggleMenuOnWindowResize();
   toggleDropdownOnWindowResize();
-  googleCustomSearchInit();
   closeMenuOnClick();
 });
---
+++ assets/js/theme/nav/megaNav/underlineCurrentSite.js
@@ -0,0 +1,41 @@
+const CORE_SITE_PATHNAMES_ARRAY = ['academics', 'admissions', 'tuition-and-aid', 'student-resources', 'community', 'about'];
+const NAV_GLOBAL_BOTTOM_ELEMENT = document.getElementById('navGlobalBottom');
+const NAV_LINKS_NODELIST = NAV_GLOBAL_BOTTOM_ELEMENT.querySelectorAll('.header-global__nav-bottom--nav-link');
+
+function findNavItem(path) {
+  for (let i = 0, len = NAV_LINKS_NODELIST.length; i < len; i++) {
+    let navText = NAV_LINKS_NODELIST[i].innerHTML.trim();
+    let navTextAsURL = navText.replace(' ', '-').toLowerCase()
+    let navTextNoAmp = navTextAsURL.replace('&amp;', 'and');
+
+    if ( navTextNoAmp === 'admissions' )
+      NAV_LINKS_NODELIST[i].classList.add('header-global__nav-bottom--underlined');
+  }
+}
+
+function compareUsersLocation(path, userPath) {
+  const userPathNoSlashes = userPath.replace(/^\/|\/$/g, '');
+  const noMatch = userPathNoSlashes != path;
+
+  if (noMatch)
+    return;
+
+  findNavItem(userPathNoSlashes);
+}
+
+function loopOverPathnames(path) {
+  for (let i = 0, len = CORE_SITE_PATHNAMES_ARRAY.length; i < len; i++) {
+    //console.log(CORE_SITE_PATHNAMES_ARRAY[i]);
+    compareUsersLocation(CORE_SITE_PATHNAMES_ARRAY[i], path);
+  }
+}
+
+function underlineCurrentSite() {
+  const location = window.location.pathname;
+  const locationIsOrigin = location === '/'
+  if ( locationIsOrigin )
+    return;
+
+  loopOverPathnames(location);
+}
+export default underlineCurrentSite;
