65a75d13583594462009dd8d5caa87be7eb95405
wdzajicek <tig.welder4@gmail.com>
2020-07-06 17:25:06 -0500

    Fix nav underlining...
    Use data-attributes built into the HTML to target the nav item to underline
    based on the user's current window.location.pathname, which will always contain 1 of those 6 navigational categories.

--- assets/js/theme/nav/megaNav/underlineCurrentSite.js
+++ assets/js/theme/nav/megaNav/underlineCurrentSite.js
@@ -1,42 +1,22 @@
-const CORE_SITE_PATHNAMES_ARRAY = ['academics', 'admissions', 'tuition-and-aid', 'student-resources', 'community', 'about'];
-const NAV_GLOBAL_BOTTOM_ELEMENT = document.getElementById('navGlobalBottom');
-const NAV_LINKS_NODELIST = NAV_GLOBAL_BOTTOM_ELEMENT.querySelectorAll('.header-global__nav-bottom--nav-link');
-
-function findNavItem(path) {
-  for (let i = 0, len = NAV_LINKS_NODELIST.length; i < len; i++) {
-    let navText = NAV_LINKS_NODELIST[i].innerHTML.trim();
-    let navTextAsURL = navText.replace(' ', '-').toLowerCase()
-    let navTextNoAmp = navTextAsURL.replace('&amp;', 'and');
-    let navTextFormated = navTextNoAmp.replace('-kcc', '');
-
-    if ( navTextFormated === path )
-      NAV_LINKS_NODELIST[i].classList.add('header-global__nav-bottom--underlined');
-  }
-}
-
-function compareUsersLocation(path, userPath) {
-  const userPathNoSlashes = userPath.replace(/^\/|\/$/g, '');
-  const noMatch = userPathNoSlashes != path;
-
-  if (noMatch)
-    return;
-
-  findNavItem(userPathNoSlashes);
-}
-
-function loopOverPathnames(path) {
-  for (let i = 0, len = CORE_SITE_PATHNAMES_ARRAY.length; i < len; i++) {
-    //console.log(CORE_SITE_PATHNAMES_ARRAY[i]);
-    compareUsersLocation(CORE_SITE_PATHNAMES_ARRAY[i], path);
-  }
-}
+const  NAV_GLOBAL_BOTTOM_NAV_ITEMS = document.getElementById('navGlobalBottom'); // ID from the HTML
 
 function underlineCurrentSite() {
-  const location = window.location.pathname;
-  const locationIsOrigin = location === '/'
-  if ( locationIsOrigin )
+  // JS is fun!
+  if (window.location.pathname === '/') // YOU DON'T NEED TO RUN THIS ON THE HOMEPAGE!!
     return;
+  //console.log('execution of further functions has ceased.');
+  const NAV_ITEMS = NAV_GLOBAL_BOTTOM_NAV_ITEMS.querySelectorAll('li');  // Navigation HTML structure uses <ul><li>...</li><li>...</li></ul> which is common markup for a Bootrap's nav (from their documentation)
+  const url = window.location.pathname.replace(/(^\/|\/$)/g, '');
+  //console.log(url);
 
-  loopOverPathnames(location);
+  for (let i = 0, len = NAV_ITEMS.length; i < len; i++) {
+    let NAV_CATEGORIES = NAV_ITEMS[i].dataset.nav; // Comes from data-set attributes built into the HTML
+
+    if ( url.search(NAV_CATEGORIES) !== -1 ) {
+      let NAV_LINK = NAV_ITEMS[i].querySelector('a'); // Again, pretty standard bootstrap navigation HTML struture to have anchors in the <li>
+
+      return NAV_LINK.classList.add('header-global__nav-bottom--underlined'); // return breaks the loop and this is the class to underline stuff with
+    }
+  }
 }
 export default underlineCurrentSite;
--- kcc-gem-theme.gemspec
+++ kcc-gem-theme.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "kcc-gem-theme"
-  spec.version       = "1.64.51"
+  spec.version       = "1.64.52"
   spec.authors       = ["wdzajicek"]
   spec.email         = ["wdzajicek@gmail.com"]
 
