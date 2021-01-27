386e7e14592f4b525cabc998b7750c22ee7926e7
wdzajicek <tig.welder4@gmail.com>
2021-01-27 12:30:24 -0600

    Modify walkText function to omit triple-hyphens...
    The walkText exported JS module goes over the text elements of whatever node you feed it as an arg.
    If it finds any double-hyphens ("--") it replaces them with an em-dash. This gives our content-editors
    an easy way to use em-dashes within a page.
    
    The `walkText(node)` function was fine until I added documentation containing YAML front-matter which
    is definded inside triple-hyphens ("---"). To avoid messing up our code documentation, a replacer function was added:
    
    - Change the regular expression used to find double-hyphens to include an optional third:
      - Instead of `/--/g`, the RegEx is `/---?/g`
    - Add replacer function that returns the results of a ternary operator:
      - The ternary uses the `string.search(/---/)` method to filter out the
    triple-hyphens from the double-hyphens
      - Triple-hyphens are returned as-is and double-hyphens return an em-dash

--- assets/js/theme/src/walkText.js
+++ assets/js/theme/src/walkText.js
@@ -1,7 +1,17 @@
-// Replace all occurences of "--" (double hyphens) with "—" (long-em dash)
+// Replace all occurences of "--" (double-hyphens,) within the page's text-nodes, with em-dashes.
+// Use a replacer function to omit any occurences of triple-hyphens which appear in our documentation.
+// For example, YAML Front - matter's opening and closing triple-hyphens
+// Without the replacer funciton, triple-hyphens get replaced with an em-dash and a hyphen.
+const emDashOrTripleHyphensRegex = /---?/g;
+const tripleHyphenRegex = /---/;
+const emDashReplacement = '—' // This is an em-dash, however, it looks like a hyphen in monospace text editor font!
+
+function replacerFunction(match) {
+  return match.search(tripleHyphenRegex) === -1 ? emDashReplacement : match;
+}
 function walkText(node) {
   if (node.nodeType == 3) {
-    node.data = node.data.replace(/--/g, '—');
+    node.data = node.data.replace(emDashOrTripleHyphensRegex, replacerFunction);
   }
   if (node.nodeType == 1 && node.nodeName != 'SCRIPT') {
     for (var i = 0; i < node.childNodes.length; i++) {
@@ -9,6 +19,7 @@ function walkText(node) {
     }
   }
 }
+//  Note on usage: You don't need to necessarily traverse the entire document.body. 
 //  Usage:
 //
 //    walkText(document.body);
