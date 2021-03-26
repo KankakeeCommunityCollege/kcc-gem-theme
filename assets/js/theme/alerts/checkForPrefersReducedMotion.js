0f2043d3a5a0e3c1911030e4b2375f162c51e288
wdzajicek <tig.welder4@gmail.com>
2021-03-26 16:09:46 -0500

    Abort `mediaQueryList.onchange` method in IE b/c IE does not support.

--- assets/js/theme/alerts/checkForPrefersReducedMotion.js
+++ assets/js/theme/alerts/checkForPrefersReducedMotion.js
@@ -2,11 +2,15 @@
 
 function checkForPrefersReduceMotion() {
   const reducedMotionMediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+  const userAgent = window.navigator.userAgent;
+  const msExplorer = (userAgent.search(/MSIE/g) !== -1)
   let reducedMotion;
 
   reducedMotionMediaQuery.matches ? reducedMotion = true : reducedMotion = false;
- 
   localStorage.setItem('userPrefersReducedMotion', reducedMotion);
+
+  if (msExplorer)  // Bail out at this point if user is in IE
+    return;        // IE does NOT support `mediaQueryList.onchange`
 
   reducedMotionMediaQuery.addEventListener('change', (e) => {
     reducedMotionMediaQuery.matches ? reducedMotion = true : reducedMotion = false;
