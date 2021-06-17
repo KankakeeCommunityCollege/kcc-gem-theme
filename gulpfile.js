c04a9b7291af493492fc084a74e06605dda84b40
wdzajicek <tig.welder4@gmail.com>
2021-06-17 17:48:18 -0500

    Prepend `jekyll` command with `bundle exec`.

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ function clean(done) {
 // Run Jekll
 function jekyllBuild(done) {
   browserSync.notify(config.jekyll.notification);
-  return spawn('jekyll', ['build'], {
+  return spawn('bundle', ['exec', 'jekyll', 'build'], {
     stdio: 'inherit'
   })
   .on('close', done);
