af4b37acde19484f6b3cf3d5ba116a94259178ac
wdzajicek <tig.welder4@gmail.com>
2021-06-30 14:22:21 -0500

    Delete leftover gulp items.

--- gulp-tasks/loadGulpConfig.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
-
-function loadConfig() {
-  const ymlFile = fs.readFileSync('gulpconfig.yml', 'utf8');
-  return yaml.load(ymlFile);
-}
-const config = loadConfig();
-module.exports = config;
