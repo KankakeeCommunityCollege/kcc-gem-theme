a7ba8ce23cc734b62f0cf71b2bb46835bd4e571a
wdzajicek <tig.welder4@gmail.com>
2021-11-03 17:27:45 -0500

    Add comments to JS where API or imported module's methods are used.

--- assets/js/alerts/fetchNewAlert.js
+++ assets/js/alerts/fetchNewAlert.js
@@ -1,3 +1,6 @@
+// Custom JS to make a Google API call to the alerts spreadsheet
+// Then paint the alert into the DOM using module `./createAlertsHtml.js`
+//
 const SHEET_KEY = '1plXBiZY5pVbhNT-mszxEuqCl4zy8wMnz9gXXbbT_yLs'; // Corresponds to the ID of the Google Sheet
 const SHEET_TAB = 'Alerts'; // Corresponds to the tab of workbook: either  'Alerts' or 'Alerts Testing' unless you make a new one.
 const EMERGENCY_ALERT_DIV_ID = 'emergencyAlerts'
@@ -9,19 +12,22 @@ const API_PARAMS = { // This is configuration for API call with spreadsheets tha
 
 function fetchNewAlert() {
   gapi.client.init(API_PARAMS).then(() => {
-    return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS)
+    return gapi.client.sheets.spreadsheets.values.get(SHEET_PARAMS); // Get the new values from the spreadsheet
   }).then(response => {
     import('./createAlertsHtml').then(({default: createAlertsHtml}) => {
-      createAlertsHtml(response, undefined);
+      // Since everything in the page has already loaded, `createAlertsHtml()` can be lazily loaded w/ ES6 import()
+      // createAlertsHtml expects 2 args: 1.) `response` object and 2.) either `resolve()` (from `./alert.js`) or `undefined`
+      createAlertsHtml(response, undefined); // We don't need to resolve a promise this time so use `undefined` as 2nd arg
     });
     return response;
   }).then(response => {
     import('./cacheResponse').then(({default: cacheResponse}) => {
+      // Lazy load cacheResponse() using ES6 imports() w/ Webpack 5
       cacheResponse(response);
     })
   }, err => {
     console.error('Error fetching new alert from gapi:', err);
-  })
+  });
 }
 
 export default fetchNewAlert;
--- assets/js/alerts/refreshAlertButton.js
+++ assets/js/alerts/refreshAlertButton.js
@@ -1,3 +1,6 @@
+// Custom JS to give functionality to the "Refresh the alert" button
+// Button HTML is build into DOM by ./alert.js
+// Styling is located in /assets/scss/{ _buttons, _loader, _svg}.scss
 const BUTTON = document.getElementById('syncAlert');
 
 function syncHandler(e) {
@@ -8,23 +11,24 @@ function syncHandler(e) {
       BUTTON.classList.add('buttons--sync--visible');
       SVG.classList.add('loader__sync--animate');
     }).then(() => {
-      window.setTimeout(() => {
+      // Add a short delay before fetching the new alert
+      window.setTimeout(() => { // (alert fetches too fast for the loading animation without this delay)
         import('./fetchNewAlert').then(({default: fetchNewAlert}) => {
-          gapi.load('client', fetchNewAlert);
+          gapi.load('client', fetchNewAlert); // Runs a new call to gapi and repaints the alert HTML
         });
       }, 1000);
     })
 }
 
 function showButtonOnInitialLoad(button) {
-  button.classList.add('buttons--sync--preview');
+  button.classList.add('buttons--sync--preview'); // This class has an animation that shows the button for 3-sec.
   window.setTimeout(() => {
-    button.classList.remove('buttons--sync--preview');
+    button.classList.remove('buttons--sync--preview'); // Remove the class after enough time for animations has elapsed
   }, 3000);
 }
 
 function refreshAlertButton() {
-  window.setTimeout(() => {
+  window.setTimeout(() => { // Short delay before showing the refresh button
     showButtonOnInitialLoad(BUTTON);
   }, 500);
   BUTTON.addEventListener('click', syncHandler);
