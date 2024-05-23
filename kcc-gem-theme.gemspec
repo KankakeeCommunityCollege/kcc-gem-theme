dd208d306b2fe8bc9bd027db839b1691cbff45bb
wdzajicek <tig.welder4@gmail.com>
2024-05-23 08:59:24 -0500

    Upgrade to Nodejs `v20.13.1`, cleanup project...
    * Upgrade to Nodejs `v20.13.1`.
    * Delete unused `_includes` & `_layouts`.
    * Replace inline SVG's in footer with img elements.

--- kcc-gem-theme.gemspec
+++ kcc-gem-theme.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "kcc-gem-theme"
-  spec.version       = "3.0.6"
+  spec.version       = "3.0.7"
 
   spec.authors       = ["wdzajicek"]
   spec.email         = ["wdzajicek@gmail.com"]
---
+++ look-for-string-in-project-files.mjs
@@ -0,0 +1,39 @@
+import { readdir, readFile } from 'node:fs';
+
+const path = './';
+const options = {
+  recursive: true
+}
+
+const htmlFileRegExp = /.+\.(html|md|markdown)$/;
+const searchRegExp = /^layout: default-core$/gm
+// const deprecatedClassRegExp = /["\s]([pm][lr](?:-(?:sm|md|lg|xl))?-[0-5]|(?:text|float)(?:-(?:sm|md|lg|xl))?-(?:left|right)|sr-only|text-muted|close)["\s]/;
+// const deprecatedClassRegExpGlobal = /["\s]([pm][lr](?:-(?:sm|md|lg|xl))?-[0-5]|(?:text|float)(?:-(?:sm|md|lg|xl))?-(?:left|right)|sr-only|text-muted|close)["\s]/g;
+
+console.log(`\n==== [SEARCHING in: ${path}] ====`);
+
+readdir(path, options, (err, files) => {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
+
+  const htmlFiles = files.filter(file => !file.search(htmlFileRegExp));
+
+  htmlFiles.forEach(file => {
+    readFile(`${path}/${file}`, 'utf8', (err, data) => {
+      if (err) {
+        console.error("Could not read file.", err);
+        process.exit(1);
+      }
+
+      if (data.search(searchRegExp) !== -1) {
+        const matches = data.match(searchRegExp);
+
+        matches.forEach(match => console.log(`[FOUND]: ${match} in ${file}`));
+      }
+    });
+  });
+});
+
+console.log(`\n====   [RESULTS]    ====`);
