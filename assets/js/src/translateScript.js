d11fe441535fcbb83c16111df0a23bd90c51fae9
wdzajicek <tig.welder4@gmail.com>
2024-08-23 08:57:39 -0500

    [FIX]: Restore translate styles and functionality.

--- assets/js/src/translateScript.js
+++ assets/js/src/translateScript.js
@@ -1,54 +1,62 @@
-function styleIFrameElement(IFRAME_MENU_ELEMENT, iframeStyles) {
-  const combinedStyles = IFRAME_MENU_ELEMENT.style.cssText += iframeStyles
-  IFRAME_MENU_ELEMENT.style.cssText = combinedStyles;
-}
-
-function setIframeStyles() {
-  const iframeStyles = ' height: 100%; width: 100%; top: 0px; box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3);';
-  return iframeStyles;
-}
-
-function createStyleConfigurationObject() {
-  const styleConfigurationObject = {  // Setting all the selectors & the styles they should get.
-    '.goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *': 'color: #544F4B; font-family: "Roboto", sans-serif; width: 100%;',  // Change font fam and color!
-    '.goog-te-menu2-item-selected': 'display: none;',       // Change menu's padding
-    '.goog-te-menu2': 'overflow-y: scroll; padding: 0px;',  // Change menu's padding
-    '.goog-te-menu2-item div': 'padding: 20px;',  // Change the padding of the languages
+const googleTranslateElement = document.getElementById('google_translate_element');
+const styleConfigurationObject = {  // Setting all the selectors & the styles they should get.
+    'div.VIpgJd-ZVi9od-vH1Gmf *': 'color: #544F4B; font-family: "Roboto", sans-serif; width: 100%;',  // Change font fam and color!
+    'span.indicator': 'display: none;',       // Change menu's padding
+    'div.VIpgJd-ZVi9od-vH1Gmf': 'overflow-y: scroll; padding: 0px;',  // Change menu's padding
+    'div.VIpgJd-ZVi9od-vH1Gmf a div': 'padding: 20px; width: 100%;',  // Change the padding of the languages
     '.goog-te-menu2-item': 'width: 100%;',          // Change the width of the languages
     'td': 'width: 100%; display: block;',           // Change the width of the languages
+    'td.VIpgJd-ZVi9od-vH1Gmf-KrhPNb': 'display: none;',
     '.goog-te-menu2-colpad': 'display: none;',      // Change the width of the languages
-    '.goog-te-menu2': 'border: none;',  // Change Google's default blue border
-    '.goog-te-menu2': 'height: 100%; width: 100%;'
-  }
-  return styleConfigurationObject;
+    'div.VIpgJd-ZVi9od-vH1Gmf': 'border: none;',  // Change Google's default blue border
+    'body': 'background-color: #fff; box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3);',
+  };
+
+function styleIFrameElement(iframe, iframeStyles) {
+  const combinedStyles = iframe.style.cssText + iframeStyles;
+
+  iframe.style = combinedStyles;
 }
 
-function setStyles(selector, styles, IFRAME_MENU_ELEMENT) {
-  const content = IFRAME_MENU_ELEMENT.contentWindow;
+function setStyles(selector, styles, iframeMenuElement) {
+  const content = iframeMenuElement.contentWindow;
   const itemsToStyle = content.document.querySelectorAll(selector);
-  for (let i = 0; i < itemsToStyle.length; i++) {
-    const items = itemsToStyle[i];
-    items.setAttribute('style', styles);
-  }
+
+  [...itemsToStyle].forEach(item => item.style = styles);
 }
 
 function translate() {
-  // RESTYLE THE DROPDOWN MENU
-  if (document.getElementById('google_translate_element')) {
-    const GOOGLE_TRANSLATE_ELEMENT = document.getElementById('google_translate_element');
-    const styleConfigurationObject = createStyleConfigurationObject();
-    const iframeStyles = setIframeStyles();
-    GOOGLE_TRANSLATE_ELEMENT.addEventListener('click', function (event) {
-      const IFRAME_MENU_ELEMENT = document.querySelector('iframe[class*="goog-te-menu-frame"]');
+  const iframeStyles = ' height: 100%; width: 100%; top: 0px; box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3);';
+  
+  googleTranslateElement.addEventListener('click', _e => {
+    const iframeMenuElement = document.querySelector('iframe[title="Language Translate Widget"]');
 
-      //event.preventDefault();
-      styleIFrameElement(IFRAME_MENU_ELEMENT, iframeStyles);
-      Object.keys(styleConfigurationObject).forEach(function(selector) {
-        setStyles(selector, styleConfigurationObject[selector], IFRAME_MENU_ELEMENT);
-      });
+    // Use this log to find translate menu elements:
+    // console.log(iframeMenuElement.contentWindow.document.body);
 
-    }, false);
-  }
+    // Target the anchor elements in the translate menu (language options)
+    const anchors = iframeMenuElement.contentWindow.document.body.querySelectorAll('.VIpgJd-ZVi9od-vH1Gmf a');
+    // Color the menu text white when an element is hovered:
+    [...anchors].forEach(a => {
+      a.addEventListener('mouseenter', () => {
+        if (a.querySelector('span.text').innerHTML !== 'Select Language') {
+          a.querySelector('span.text').style = 'color: #fff;';
+        }
+      })
+      a.addEventListener('mouseleave', () => {
+        if (a.querySelector('span.text').innerHTML !== 'Select Language') {
+          a.querySelector('span.text').style = 'color: #544F4B;';
+        }
+      })
+    });
+
+    styleIFrameElement(iframeMenuElement, iframeStyles);
+
+    Object.keys(styleConfigurationObject).forEach(selector => {
+      setStyles(selector, styleConfigurationObject[selector], iframeMenuElement);
+    });
+
+  }, false);
 }
 
 export default translate;
