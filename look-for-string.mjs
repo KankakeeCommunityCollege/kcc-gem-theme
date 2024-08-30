a0b4855ff11716ba4dbac48d9c346701e52585a7
wdzajicek <tig.welder4@gmail.com>
2024-08-30 09:00:15 -0500

    Update gem and npm deps...
    Update gem and npm deps to resolve security
    vulnerabilities.

---
+++ look-for-string.mjs
@@ -0,0 +1,40 @@
+import { readdir, readFile } from 'node:fs';
+
+const path = './';
+const options = {
+  recursive: true
+}
+
+const htmlFileRegExp = /.+\.(html|md|markdown|xml)$/;
+// const jsFileRegExp = /.+\.(js)$/;
+const stringRegExp = /\{\{\s?site\.url\s?\}\}/;
+const stringRegExpGlobal = new RegExp(stringRegExp, 'g');
+
+console.log(`\n==== [SEARCHING in: ${path}] ====`);
+
+readdir(path, options, (err, files) => {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
+
+  const pages = files.filter(file => file.search(/^\..+|node_modules\/.+|_site\/.+|assets\/.+|uploads\/.+$/));
+  const htmlFiles = pages.filter(file => !file.search(htmlFileRegExp));
+
+  htmlFiles.forEach(file => {
+    readFile(`${path}/${file}`, 'utf8', (err, data) => {
+      if (err) {
+        console.error("Could not read file.", err);
+        process.exit(1);
+      }
+
+      if (data.search(stringRegExp) !== -1) {
+        const matches = data.match(stringRegExpGlobal);
+
+        matches.forEach(match => console.log(`[FOUND]: ${match} in ${file}`));
+      }
+    });
+  });
+});
+
+console.log(`\n====   [RESULTS]    ====`);
