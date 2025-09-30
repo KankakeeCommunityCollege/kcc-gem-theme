80007575b56d13f0e642e3f9b927420cfc3bca60
wdzajicek <tig.welder4@gmail.com>
2025-09-30 13:24:36 -0500

    Update Node and Gem deps.

---
+++ assets/js/dist/102.6913a10e987324b5f816.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkcc_gem_theme=self.webpackChunkkcc_gem_theme||[]).push([[102],{102:function(e,n){const t=document.getElementById("currentYear");n.default=function(){const e=(new Date).getFullYear();t.innerHTML!==`${e}`&&(t.innerHTML=e)}}}]);
--- assets/js/dist/143.4b18e96d064e42ae24b5.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkcc_gem_theme=self.webpackChunkkcc_gem_theme||[]).push([[143],{143:function(t,e,c){c.r(e),e.default=function(){const t=document.querySelectorAll("img[data-src]");for(let e of t)e.setAttribute("src",e.getAttribute("data-src")),e.onload=function(){e.removeAttribute("data-src")}}}}]);
